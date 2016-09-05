4a039253f9aa42754eaaf4b423c9accd38a3f695
dayudodo <dayudodo@gmail.com>
2016-09-05 23:50:40 +0800

    update 1.4.1

--- server/exportProducts.js
+++ server/exportProducts.js
@@ -8,8 +8,8 @@ export function exportProducts(){
 	let inventoryArr = ['小西门', '老店'] // 有几个仓库名称
 	inventoryArr.forEach(inventory_name=>{
 		let exportFileName = `${absoluteBasePath}/server/output/products${inventory_name}.xlsx`
-		let nameReg = new RegExp(inventory_name)
-		manualSaleArr = Products.find({importSource: nameReg})
+		// let nameReg = new RegExp(inventory_name)
+		manualSaleArr = Products.find({importSource: /`${inventory_name}`/}).fetch()
 		console.log(_.first(manualSaleArr))
 		// let header = ['国际条码','产品名称','商品规格','库存']
 		// exportToExcel({
